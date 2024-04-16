f06d81680355065457d0a02146aa3f101c7b5161
paulopezcastillon <pau.lopez.castillon@gmail.com>
2024-04-16 14:00:37 +0200

    perspection_div+test dir

--- tests/opengl/lib/main.cc
+++ tests/opengl/lib/main.cc
@@ -9,38 +9,6 @@
 #include "GLSC2/binary.c"
 #include "test/test.h"
 
-#define CL_CHECK(_expr)                                                \
-   do {                                                                \
-     cl_int _err = _expr;                                              \
-     if (_err == CL_SUCCESS)                                           \
-       break;                                                          \
-     printf("OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err);   \
-     exit(-1);                                                         \
-   } while (0)
-
-#define CL_CHECK2(_expr)                                               \
-   ({                                                                  \
-     cl_int _err = CL_INVALID_VALUE;                                   \
-     decltype(_expr) _ret = _expr;                                     \
-     if (_err != CL_SUCCESS) {                                         \
-       printf("OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err); \
-       exit(-1);                                                       \
-     }                                                                 \
-     _ret;                                                             \
-   })
-
-
-#define TEST(func)                                                     \
-  ({                                                                   \
-    char function_name[] = #func;                                      \
-    printf("Running %s.\n",function_name);                             \
-    uint32_t result = func();                                          \
-    if (!result) printf("PASSED.\n");                                  \
-    else printf("FAILED with %d errors.\n", result);                   \
-    errors += result;                                                  \
-  })
-
-
 static bool almost_equal(float a, float b, int ulp = 4) {
   union fi_t { int i; float f; };
   fi_t fa, fb;
@@ -69,8 +37,8 @@ int main (int argc, char **argv) {
 
   uint32_t errors = 0;
   TEST(test_perspective_div);
-  // TEST(test_color_kernel);
-  // TEST(test_color_kernel_discard_true);
+  TEST(test_color_kernel);
+  TEST(test_color_kernel_discard_true);
 
   // CLEANUP
   if (context) clReleaseContext(context);
